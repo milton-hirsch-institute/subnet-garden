6668f53ce4e397b3e43ea0c2cf4174fb0b75e460
Rafe Kaplan <766471+slobberchops@users.noreply.github.com>
2023-12-20 17:47:18 +0100

    Make the memory garden deserializable

--- core/src/memory.rs
+++ core/src/memory.rs
@@ -368,7 +368,7 @@ impl<'s> serde::Deserialize<'s> for MemorySpace {
     }
 }
 
-#[derive(serde::Serialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 pub struct MemorySubnetGarden {
     spaces: HashMap<String, MemorySpace>,
 }
