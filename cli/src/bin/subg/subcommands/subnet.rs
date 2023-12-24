13ae3bd7287b676c88b27ea112857aeac7c7918a
Rafe Kaplan <766471+slobberchops@users.noreply.github.com>
2023-12-24 18:49:08 +0100

    Implemented free command

--- cli/src/bin/subg/subcommands/subnet.rs
+++ cli/src/bin/subg/subcommands/subnet.rs
@@ -3,7 +3,7 @@
 
 use crate::args::{AllocateArgs, FreeArgs, SubgArgs};
 use cidr::IpCidr;
-use subnet_garden_core::Space;
+use std::process::exit;
 
 pub(crate) fn allocate(subg: &SubgArgs, args: &AllocateArgs) {
     let mut garden = crate::load_garden(&subg.garden_path);
@@ -17,7 +17,7 @@ pub(crate) fn allocate(subg: &SubgArgs, args: &AllocateArgs) {
 
 pub(crate) fn free(subg: &SubgArgs, args: &FreeArgs) {
     let mut garden = crate::load_garden(&subg.garden_path);
-    let cidr = match garden.find_by_name(&args.identifier.as_deref().unwrap()) {
+    let cidr = match garden.find_by_name(&args.identifier.as_str()) {
         Some(cidr) => cidr,
         None => crate::result(
             args.identifier.parse::<IpCidr>(),
@@ -25,7 +25,11 @@ pub(crate) fn free(subg: &SubgArgs, args: &FreeArgs) {
             "Could not parse arg IDENTIFIER",
         ),
     };
-    garden
+    if !garden.free(&cidr) {
+        eprintln!("Could not free subnet {}", cidr);
+        exit(exitcode::SOFTWARE);
+    }
+    crate::store_space(&subg.garden_path, &garden);
 }
 
 #[cfg(test)]
@@ -33,47 +37,108 @@ mod tests {
     use super::*;
     use crate::tests::*;
 
-    fn new_allocate_test(bits: &str, name: Option<&str>) -> Test {
-        let mut test = tests::new_test();
-        test.store();
-        test.subg.arg("allocate").arg(bits);
-        if let Some(name) = name {
-            test.subg.arg(name);
+    mod allocate {
+        use super::*;
+        fn new_allocate_test(bits: &str, name: Option<&str>) -> Test {
+            let mut test = tests::new_test();
+            test.store();
+            test.subg.arg("allocate").arg(bits);
+            if let Some(name) = name {
+                test.subg.arg(name);
+            }
+            test
         }
-        test
+
+        #[test]
+        fn allocate_failure() {
+            let mut test = new_allocate_test("8", Some("test"));
+            test.garden.allocate(16, None).unwrap();
+            test.store();
+            test.subg
+                .assert()
+                .failure()
+                .code(exitcode::SOFTWARE)
+                .stdout("")
+                .stderr("Could not allocate subnet\nNo space available\n");
+        }
+
+        #[test]
+        fn allocate_with_name() {
+            let mut test = new_allocate_test("8", Some("test"));
+            test.subg.assert().success().stdout("").stderr("");
+            test.load();
+            let subnets = test.garden.entries().to_vec();
+            assert_eq!(subnets.len(), 1);
+            assert_eq!(subnets[0].name.clone().unwrap(), "test");
+            assert_eq!(subnets[0].cidr.to_string(), "10.10.0.0/24");
+        }
+
+        #[test]
+        fn allocate_without_name() {
+            let mut test = new_allocate_test("8", None);
+            test.subg.assert().success().stdout("").stderr("");
+            test.load();
+            let subnets = test.garden.entries().to_vec();
+            assert_eq!(subnets.len(), 1);
+            assert_eq!(subnets[0].name, None);
+            assert_eq!(subnets[0].cidr.to_string(), "10.10.0.0/24");
+        }
     }
 
-    #[test]
-    fn allocate_failure() {
-        let mut test = new_allocate_test("8", Some("test"));
-        test.garden.allocate(16, None).unwrap();
-        test.store();
-        test.subg
-            .assert()
-            .failure()
-            .code(exitcode::SOFTWARE)
-            .stdout("")
-            .stderr("Could not allocate subnet\nNo space available\n");
-    }
-    #[test]
-    fn allocate_with_name() {
-        let mut test = new_allocate_test("8", Some("test"));
-        test.subg.assert().success().stdout("").stderr("");
-        test.load();
-        let subnets = test.garden.entries().to_vec();
-        assert_eq!(subnets.len(), 1);
-        assert_eq!(subnets[0].name.clone().unwrap(), "test");
-        assert_eq!(subnets[0].cidr.to_string(), "10.10.0.0/24");
-    }
+    mod free {
+        use super::*;
+        fn new_free_test(identifier: &str) -> Test {
+            let mut test = tests::new_test();
+            test.store();
+            test.subg.arg("free").arg(identifier);
+            test
+        }
 
-    #[test]
-    fn allocate_without_name() {
-        let mut test = new_allocate_test("8", None);
-        test.subg.assert().success().stdout("").stderr("");
-        test.load();
-        let subnets = test.garden.entries().to_vec();
-        assert_eq!(subnets.len(), 1);
-        assert_eq!(subnets[0].name, None);
-        assert_eq!(subnets[0].cidr.to_string(), "10.10.0.0/24");
+        #[test]
+        fn name_not_found() {
+            let mut test = new_free_test("test");
+            test.store();
+            test.subg
+                .assert()
+                .failure()
+                .code(exitcode::USAGE)
+                .stdout("")
+                .stderr(
+                    "Could not parse arg IDENTIFIER\n\
+                couldn\'t parse address in network: invalid IP address syntax\n",
+                );
+        }
+
+        #[test]
+        fn free_failure() {
+            let mut test = new_free_test("20.20.0.0/24");
+            test.store();
+            test.subg
+                .assert()
+                .failure()
+                .code(exitcode::SOFTWARE)
+                .stdout("")
+                .stderr("Could not free subnet 20.20.0.0/24\n");
+        }
+
+        #[test]
+        fn free_success_with_name() {
+            let mut test = new_free_test("test");
+            test.garden.allocate(4, Some("test")).unwrap();
+            test.store();
+            test.subg.assert().success().stdout("").stderr("");
+            test.load();
+            assert_eq!(test.garden.find_by_name("test"), None);
+        }
+
+        #[test]
+        fn free_success_with_cidr() {
+            let mut test = new_free_test("10.10.0.0/28");
+            test.garden.allocate(4, Some("test")).unwrap();
+            test.store();
+            test.subg.assert().success().stdout("").stderr("");
+            test.load();
+            assert_eq!(test.garden.find_by_name("test"), None);
+        }
     }
 }
