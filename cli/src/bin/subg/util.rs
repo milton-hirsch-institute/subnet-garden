08e56aef0a43d8d78ba7b98e5e3a48ab5c25556e
Rafe Kaplan <766471+slobberchops@users.noreply.github.com>
2024-01-08 22:55:43 +0100

    a little test coverage for the right pad function

--- cli/src/bin/subg/util.rs
+++ cli/src/bin/subg/util.rs
@@ -6,3 +6,22 @@ pub(crate) fn right_pad(s: &mut String, width: usize) {
         s.push(' ');
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_right_pad() {
+        let mut s = String::from("foo");
+        right_pad(&mut s, 5);
+        assert_eq!(s, "foo  ");
+    }
+
+    #[test]
+    fn test_right_pad_too_wide() {
+        let mut s = String::from("foo-bar");
+        right_pad(&mut s, 5);
+        assert_eq!(s, "foo-bar");
+    }
+}
