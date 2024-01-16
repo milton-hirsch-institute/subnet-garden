638107ed10e8fb641fd1ebde6820d01355fbd29c
Rafe Kaplan <766471+slobberchops@users.noreply.github.com>
2024-01-16 23:33:21 +0100

    Implemented combinatorial list iterator

--- cli/src/bin/subg/util/iter.rs
+++ cli/src/bin/subg/util/iter.rs
@@ -1,8 +1,8 @@
 // Copyright 2024 The Milton Hirsch Institute, B.V.
 // SPDX-License-Identifier: Apache-2.0
+#![allow(dead_code)]
 
-use std::slice::Iter;
-
+#[derive(Debug, PartialEq)]
 struct ListCombinationIterator<T>
 where
     T: Clone + Default,
@@ -16,18 +16,26 @@ impl<A> ListCombinationIterator<A>
 where
     A: Clone + Default,
 {
-    fn new(list: Vec<Vec<A>>) -> ListCombinationIterator<A> {
+    fn new(list: Vec<Vec<A>>) -> Option<ListCombinationIterator<A>> {
+        if list.is_empty() {
+            return None;
+        }
+        for sublist in list.iter() {
+            if sublist.is_empty() {
+                return None;
+            }
+        }
         let mut indexes: Vec<usize> = Vec::new();
         let mut result: Vec<A> = Vec::new();
-        for _ in 0..list.len() {
+        for sublist in list.iter() {
             indexes.push(0);
-            result.push(A::default());
+            result.push(sublist[0].clone());
         }
-        ListCombinationIterator {
+        Some(ListCombinationIterator {
             list,
             indexes,
             result,
-        }
+        })
     }
 }
 
@@ -38,22 +46,56 @@ where
     type Item = Vec<A>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        for index in self.list.len() - 1..=0 {
-            let list = &self.list[index];
+        for index in (0..self.list.len()).rev() {
+            let sublist = &self.list[index];
             let list_index = self.indexes[index];
-            if list_index < list.len() {
-                self.result[index] = list[list_index].clone();
-                self.indexes[index] += 1;
+            if list_index < sublist.len() {
+                self.result[index] = sublist[list_index].clone();
+                let indexes_length = self.indexes.len();
+                self.indexes[indexes_length - 1] += 1;
                 break;
             } else {
                 if index == 0 {
                     return None;
                 }
                 self.indexes[index] = 0;
-                self.result[index] = list[0].clone();
+                self.result[index] = sublist[0].clone();
+                self.indexes[index - 1] += 1;
             }
         }
 
         Some(self.result.clone())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn has_empty_list() {
+        let list = vec![vec![1, 2], Vec::<i32>::new(), vec![5, 6]];
+        assert_eq!(ListCombinationIterator::<i32>::new(list), None);
+    }
+
+    #[test]
+    fn has_no_lists() {
+        let list = Vec::<Vec<i32>>::new();
+        assert_eq!(ListCombinationIterator::new(list), None);
+    }
+
+    #[test]
+    fn test_list_combination_iterator() {
+        let list = vec![vec![1, 2], vec![3, 4], vec![5, 6]];
+        let mut iter = ListCombinationIterator::new(list).unwrap();
+        assert_eq!(iter.next(), Some(vec![1, 3, 5]));
+        assert_eq!(iter.next(), Some(vec![1, 3, 6]));
+        assert_eq!(iter.next(), Some(vec![1, 4, 5]));
+        assert_eq!(iter.next(), Some(vec![1, 4, 6]));
+        assert_eq!(iter.next(), Some(vec![2, 3, 5]));
+        assert_eq!(iter.next(), Some(vec![2, 3, 6]));
+        assert_eq!(iter.next(), Some(vec![2, 4, 5]));
+        assert_eq!(iter.next(), Some(vec![2, 4, 6]));
+        assert_eq!(iter.next(), None);
     }
 }
